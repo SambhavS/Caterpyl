e2af1d3f249f1e41d3102a26ad70b77675b902f3
sambhav <sambhav.reddy@gmail.com>
2019-01-23 14:11:58 -0800

    AST first draft

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ Formal Grammar
 <function> ::= "int" <id> "(" ")" "{" <statement> "}"
 <statement> ::= "return" <exp> ";"
 <exp> ::= <int>
+
+Todo: -type checking
+    
 """
+
+##############################
+
 VAR_CHARS = string_lib.ascii_letters + string_lib.digits + "_"
 SPECIAL_CHARS  = "(){;}"
 DIGITS = string_lib.digits
@@ -14,6 +20,7 @@ WHITESPACE = " \n\t"
 class Tkn:
     """Token name class"""
     var = "VARIABLE"
+    binop = "BINARY OPERATOR"
 
     ifx = "IF KEYWORD"
     elsex = "ELSE KEYWORD"
@@ -37,7 +44,7 @@ class Tkn:
     intx = "INT"
     floatx = "FLOAT"
     charx = "CHAR"
-    data_types = (intx, floatx, charx)
+    constants = (intx, floatx, charx)
 
 def is_float(token):
     try:
@@ -57,6 +64,9 @@ def is_char(token):
 def is_var(token):
     return all([c in VAR_CHARS for c in token])
 
+def is_bin_op(token):
+    return token in ("*", "+", "-", "/")
+
 def tok_type(token):
     if   token == "if":     return Tkn.ifx
     elif token == "else":   return Tkn.elsex
@@ -74,8 +84,13 @@ def tok_type(token):
     elif is_int(token):     return Tkn.intx
     elif is_float(token):   return Tkn.floatx
     elif is_char(token):    return Tkn.charx
+    elif is_bin_op(token):  return Tkn.binop
     elif is_var(token):     return Tkn.var
     
+
+##############################
+
+
 # AST Nodes
 class Prog:
     def __init__(self, subtrees):
@@ -126,8 +141,9 @@ class Assign:
         self.val = val
 
 class Const:
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, val, val_type):
+        self.val_type = val_type
+        self.val = val
 
 class Return:
     def __init__(self, ret_val):
@@ -139,9 +155,18 @@ class Var:
     def __init__(self, var_name):
         self.var_name = var_name
 
-class Op:
-    def __init__(self, op_name):
+class Operation:
+    def __init__(self, op_name, exp1, exp2):
+        self.children = []
+        self.exp1 = exp1
+        self.exp2 = exp2
+        attach(self, exp1)
+        attach(self, exp2)
         self.op_name = op_name
+
+
+##############################
+
 
 # Functions
 def attach(parent, child):
@@ -157,7 +182,7 @@ def tokenize(string):
     token = ""
     for i, c in enumerate(string):
         token += c
-        is_final_char = c in Tkn.special_chars or i+1 == len(string) or string[i+1] in SPECIAL_CHARS + WHITESPACE
+        is_final_char = c in SPECIAL_CHARS or i+1 == len(string) or string[i+1] in SPECIAL_CHARS + WHITESPACE
         if is_final_char:
             tokens.append(token)
             token = ""
@@ -184,6 +209,57 @@ def parse_statements(tokens, ind):
     ind += 1
     return statements, ind
 
+def expression_AST(tokens, ind):
+    final_ind = ind
+    tok_list = []
+    p_lev = 0
+    while tok_type(tokens[final_ind]) is not Tkn.semicolon: 
+        """REFACTOR VERY UGLY"""
+        curr_type = tok_type(tokens[final_ind])
+        if curr_type is Tkn.lparen:
+            p_lev -=1
+        elif curr_type is Tkn.rparen:
+            p_lev += 1
+        if  p_lev > 0:
+            final_ind += 1
+            break
+        tok_list.append(tokens[final_ind])
+        final_ind += 1
+
+    lst_ind = 0
+    first_tkn_type = tok_type(tok_list[lst_ind])
+    if first_tkn_type in Tkn.constants:
+        constant = Const(tok_list[lst_ind], first_tkn_type)
+        lst_ind += 1
+        if len(tok_list) == 1:
+            return constant, final_ind
+        elif tok_type(tok_list[lst_ind]) is Tkn.binop:
+            operator = tok_list[lst_ind]
+            lst_ind += 1
+            second_expression, _ = expression_AST(tokens, ind + lst_ind)
+            return Operation(op_name = operator, exp1=constant, exp2=second_expression), final_ind
+        else:
+            raise Exception("Constant followed by nonterminary token that is not nonbinary operator")
+
+    elif first_tkn_type is Tkn.lparen:
+        lst_ind += 1
+        p_lev = -1
+        while p_lev < 0:
+            if tok_type(tok_list[lst_ind]) is Tkn.rparen:
+                p_lev += 1
+            elif tok_type(tok_list[lst_ind]) is Tkn.lparen:
+                p_lev -= 1
+            lst_ind += 1
+        if len(tok_list) == lst_ind:
+            subtree, _ = expression_AST(tokens, ind+1)
+            return subtree, final_ind
+        elif tok_type(tok_list[lst_ind]) is tkn.binop:
+            operator = tok_type(tok_list[lst_ind])
+            expression1, _ = expression_AST(tokens, ind+1)
+            expression2, _ = expression_AST(tokens, ind+lst_ind)
+            return Operation(op_name=Operation, exp1=expression1, exp2=expression2), final_ind
+    print(tokens, ind, first_tkn_type)
+
 
 def sub_AST(tokens, ind):
     root = tokens[ind]
@@ -194,8 +270,8 @@ def sub_AST(tokens, ind):
         if root_type is Tkn.ifx:
             ind += 1
             check(tok_type(tokens[ind]), Tkn.lparen, "if should be followed by '('")
-            cond = Cond(operator = Op(tokens[ind+2]), operand1=Var(tokens[ind+1]), operand2=Var(tokens[ind+3]))
-            ind += 4
+            ind += 1
+            expression, ind = expression_AST(tokens, ind)
             check(tok_type(tokens[ind]), Tkn.rparen, "missing closing parenthesis")
             ind += 1
             true_body, ind = parse_statements(tokens, ind)
@@ -204,11 +280,11 @@ def sub_AST(tokens, ind):
                 else_body, ind = parse_statements(tokens, ind)
             else:
                 else_body = None
-            subtree = If(cond=cond, true_body=Body(true_body), else_body=Body(else_body))
+            subtree = If(cond=expression, true_body=Body(true_body), else_body=Body(else_body))
         elif root_type is Tkn.returnx:
             ind += 1
-            # WHAT IF not const?
-            subtree = Return(Const(tokens[ind]))
+            ret_expression, ind = expression_AST(tokens, ind)
+            subtree = Return(ret_expression)
 
     elif root_type in Tkn.type_keys:
         """Function Declaration"""
@@ -229,8 +305,8 @@ def sub_AST(tokens, ind):
         ind += 1
         check(tok_type(tokens[ind]), Tkn.equal, "missing = after variable name")
         ind += 1
-        value = Const(tokens[ind])
-        subtree = Assign(var_name=variable, val=value)
+        expression, ind = expression_AST(tokens, ind)
+        subtree = Assign(var_name=variable, val=expression)
     return subtree, ind
 
 def main_AST(program):
@@ -242,25 +318,23 @@ def main_AST(program):
         subtrees.append(subtree)
     return Prog(subtrees)
 
-
-
-
-
 def print_tree(tree):
-    print()
     print("Parent: ", tree.__class__.__name__)
     if hasattr(tree, "children"):
         children = tree.children
         for child in children:
-            print("Child:", child.__class__.__name__)
+            print("-Child: {}".format(child.__class__.__name__)) 
+            if hasattr(child, "val"):
+                print("  Val: {}".format(child.val))
+            if hasattr(child, "op_name"):
+                print("  Operator: {}".format(child.op_name))
         for child in children:
             print_tree(child)
-        
+
+#print(tokenize("5 + (10 + 10);"))
+print_tree(expression_AST(tokenize("5 + 10;"), 0)[0])
 
 
-
-ast = main_AST("int main(){ return 1; } ")
-print_tree(ast)
 
 
 
